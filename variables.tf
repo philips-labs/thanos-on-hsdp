2fc963f1f581e2ed890af2f7ceb5d43ddbca720f
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-17 17:08:30 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,9 +4,6 @@ variable "cf_org_name" {}
 variable "cf_space_name" {}
 variable "region" {}
 
-variable "us_east_ct_oauth2_client_id" {}
-variable "us_east_ct_oauth2_password" {}
-
 variable "grafana_password" {
   type = string
 }
