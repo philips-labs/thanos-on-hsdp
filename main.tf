a4db04c9298f7380b0068b1641801061181a38f9
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-19 10:44:47 +0200

    Update module

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "thanos" {
   source  = "philips-labs/thanos/cloudfoundry"
-  version = "2.0.1"
+  version = "2.0.2"
 
   cf_org_name   = var.cf_org_name
   cf_space_name = var.cf_space_name
+  grafana_password = var.grafana_password
 
   // needed for paas_prometheus_exporter
   environment = {
