30c1c018a8c4ade5e4b6a77816b1e758c4f9bcb1
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-19 10:55:25 +0200

    Formatting

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ module "thanos" {
   source  = "philips-labs/thanos/cloudfoundry"
   version = "2.0.2"
 
-  cf_org_name   = var.cf_org_name
-  cf_space_name = var.cf_space_name
+  cf_org_name      = var.cf_org_name
+  cf_space_name    = var.cf_space_name
   grafana_password = var.grafana_password
 
   // needed for paas_prometheus_exporter
   environment = {
-    USERNAME     = var.cf_username
-    PASSWORD     = var.cf_password
-    API_ENDPOINT = data.hsdp_config.cf.url
+    USERNAME           = var.cf_username
+    PASSWORD           = var.cf_password
+    API_ENDPOINT       = data.hsdp_config.cf.url
     ENABLE_CF_EXPORTER = true
   }
 
